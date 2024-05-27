839813040705a4ff13dd4d83690b9ffcca67fb58
KINGO <spental12345@gmail.com>
2024-05-28 00:44:23 +0300

    Enabled CORS

--- src/init-app.ts
+++ src/init-app.ts
@@ -4,5 +4,6 @@ import * as cookieParser from 'cookie-parser';
 export function initApp(app: INestApplication<any>) {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 }
