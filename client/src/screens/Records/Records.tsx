e76104e56d13c0b283cce71e052a1402cc8f3215
KINGO <spental12345@gmail.com>
2024-06-25 20:02:35 +0300

    Refac

--- client/src/screens/Records/Records.tsx
+++ client/src/screens/Records/Records.tsx
@@ -1,11 +1,18 @@
 import SelectRecordsLocation from "./SelectRecordsLocation";
 import RecordsWrapper from "./RecordsWrapper";
+import Record from "./Record";
 
 const Records = () => {
   return (
     <div>
       <SelectRecordsLocation />
-      <RecordsWrapper />
+      <RecordsWrapper>
+        <Record userName="KINGO" steamId="76561198990669262" group={"WHITELIST"} authorName="author1" expirationDate={new Date(Date.now() + 1000 * 60 * 60 * 25 * 1.023 )}/>
+        <Record userName="aboba" steamId="76561198061788436" group={"MODARATOR"} authorName="author3" />
+        <Record userName="aboba" steamId="76561198088834273" group={"GAME MASTER"} authorName="author3" />
+        <Record userName="aboba" steamId="76561198201646397" group={"GUEST"} authorName="author3" />
+        <Record userName="aboba" steamId="76561198888290148" group={"CLANWHITELIST"} authorName="author3" />
+      </RecordsWrapper>
     </div>
   )
 }
--- client/src/screens/Records/RecordsWrapper.tsx
+++ client/src/screens/Records/RecordsWrapper.tsx
@@ -1,18 +1,12 @@
 import styles from './Records.module.scss';
-
-import { FC } from "react";
-import Record from "./Record";
+import { FC, PropsWithChildren } from "react";
 import CreateRecord from "./CreateRecord";
 
-const RecordsWrapper: FC = () => {
+const RecordsWrapper: FC<PropsWithChildren> = ({children}) => {
   return (
     <div className={styles.recordsWrapper}>
       <CreateRecord />
-      <Record userName="KINGO" steamId="76561198990669262" group={"WHITELIST"} authorName="author1" expirationDate={new Date(Date.now() + 1000 * 60 * 60 * 25 * 1.023 )}/>
-      <Record userName="aboba" steamId="76561198061788436" group={"MODARATOR"} authorName="author3" />
-      <Record userName="aboba" steamId="76561198088834273" group={"GAME MASTER"} authorName="author3" />
-      <Record userName="aboba" steamId="76561198201646397" group={"GUEST"} authorName="author3" />
-      <Record userName="aboba" steamId="76561198888290148" group={"CLANWHITELIST"} authorName="author3" />
+      {children}
     </div>
   )
 }
