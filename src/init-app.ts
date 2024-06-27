50fe79502e06f0eaefe66b94b5843b3e858d2716
KINGO <spental12345@gmail.com>
2024-06-27 23:14:04 +0300

    Added global prefix init

--- src/init-app.ts
+++ src/init-app.ts
@@ -1,8 +1,9 @@
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import * as cookieParser from 'cookie-parser';
+import config from "./config";
 
 export function initApp(app: INestApplication<any>) {
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(config.API_PREFIX);
   app.use(cookieParser());
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
