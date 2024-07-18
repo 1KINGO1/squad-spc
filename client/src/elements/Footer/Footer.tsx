6fa9f786d209d728423bcd82b50d70cea37a9103
KINGO <spental12345@gmail.com>
2024-07-18 22:58:57 +0300

    added version to the footer

--- client/src/elements/Footer/Footer.tsx
+++ client/src/elements/Footer/Footer.tsx
@@ -7,14 +7,14 @@ const Footer = () => {
       <p className={styles.developedBy}>
         Developed by <a href="https://github.com/1KINGO1/squad-spc" target="_SEJ" rel="noreferrer">KINGO</a>
       </p>
-      <p className={styles.sponsoredBy}>
-        Sponsored by <a href="https://discord.gg/4AHFntZDTc" target="_SEJ" rel="noreferrer">HQ Ukraine</a> Squad Server
-      </p>
       <p className={styles.inspiredBy}>
         Inspired by Squad_Whitelister
       </p>
       <p className={styles.bottomText}>
         OpenSource © 2024
+      </p>
+      <p className={styles.version}>
+        Beta 0.0.1
       </p>
     </footer>
   )
