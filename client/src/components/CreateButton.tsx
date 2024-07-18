bab92ed196244a95fa352a0d957fcd27ecc1be7f
KINGO <spental12345@gmail.com>
2024-07-19 02:11:02 +0300

    Changed add button position

--- client/src/components/CreateButton.tsx
+++ client/src/components/CreateButton.tsx
@@ -16,7 +16,7 @@ const CreateButton: FC<CreateButtonProps> = ({onClick, style}) => {
                  type="primary"
                  onClick={onClick}
                  shape="square"
-                 style={{...style, position: "fixed", bottom: "10px", right: "10px"}}
+                 style={{...style, position: "fixed", bottom: "30px", right: "30px"}}
                  className={styles.addClanButton}
     />
   )
