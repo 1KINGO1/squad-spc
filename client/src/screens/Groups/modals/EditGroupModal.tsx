2e705121aab71360b1455183e702719098367d79
KINGO <spental12345@gmail.com>
2024-07-19 20:20:35 +0300

    Fixed incorrect group update form initial values

--- client/src/screens/Groups/modals/EditGroupModal.tsx
+++ client/src/screens/Groups/modals/EditGroupModal.tsx
@@ -1,14 +1,14 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 
-import { Button, Form, Input, message, Modal, Select } from "antd";
+import { Button, Form, message, Modal} from "antd";
 
 import IFormValues from "./shared/IFormValues";
 import usePermissions from "../../../hooks/usePermissions";
 import useUpdateGroup from "../../../hooks/useUpdateGroup";
 import GroupType from "../../../types/models/Group";
 import Permission from "../../../types/models/Permission";
-import useGroups from "../../../hooks/useGroups";
-import Group from "../../../types/models/Group";
+import GroupForm from "./shared/GroupForm";
+
 
 interface EditGroupModalProps {
   isOpen: boolean;
@@ -16,58 +16,52 @@ interface EditGroupModalProps {
   group: GroupType;
 }
 
-const validateGroupName = (groups: Group[]) => (_: any, value: string) => {
-  if (groups.find((group) => group.name === value)) {
-    return Promise.reject("Group with this name already exists");
-  }
-  return Promise.resolve();
-}
-
 const EditGroupModal: FC<EditGroupModalProps> = (props) => {
   const [isLoading, setIsLoading] = useState(false);
   const [submittable, setSubmittable] = useState(false);
-
-  const {permissions} = usePermissions();
-  const {groups} = useGroups();
-
+  const { permissions } = usePermissions();
   const [form] = Form.useForm<IFormValues>();
   const values = Form.useWatch([], form);
 
   const mutation = useUpdateGroup({
     onSuccess: () => {
+      form.resetFields();
+      setSubmittable(false);
+      setIsLoading(false);
       props.setIsOpen(false);
     },
     onError: (err) => {
       message.error(err);
     }
-  })
+  });
+  const initialValues = useMemo(() => {
+    return { name: props.group.name, permissions: props.group.permissions.map(p => p.value) };
+  }, [props.group])
 
   const handleCancel = () => {
-    form.resetFields();
     props.setIsOpen(false);
-  }
-
+  };
   const handleOk = () => {
     mutation.mutate({
       id: props.group.id,
       name: values.name === props.group.name ? undefined : values.name,
       permissions: values.permissions.map(permission => (permissions.find(p => p.value === permission) as Permission).id)
-    })
+    });
     props.setIsOpen(false);
-  }
+  };
 
   useEffect(() => {
     form
       .validateFields({ validateOnly: true })
       .then((values) => {
-        if (values.name === props.group.name && values.permissions + "" === props.group.permissions + "") {
+        if (values.name === props.group.name && values.permissions + "" === props.group.permissions.map(p => p.value) + "") {
           setSubmittable(false);
           return;
         }
         setSubmittable(true);
       })
       .catch(() => setSubmittable(false));
-  }, [values, form])
+  }, [values, form]);
 
   return (
     <Modal
@@ -88,44 +82,12 @@ const EditGroupModal: FC<EditGroupModalProps> = (props) => {
         </Button>
       ]}
     >
-      <Form
+      <GroupForm
         form={form}
-        layout="vertical"
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 20 }}
-        initialValues={{
-          name: props.group.name,
-          permissions: props.group.permissions.map(({ value }) => value)
-        }}
-      >
-        <Form.Item
-          layout="vertical"
-          label="Group Name"
-          name="name"
-          rules={[{ max: 50, min: 4, required: true }, {validator: validateGroupName(groups)}]}
-          labelCol={{ span: 24 }}
-          wrapperCol={{ span: 24 }}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          layout="vertical"
-          label="Group Permissions"
-          name="permissions"
-          rules={[{ required: true, type: "array", min: 1, message: "Please select at least one permission"}]}
-          labelCol={{ span: 24 }}
-          wrapperCol={{ span: 24 }}
-        >
-          <Select
-            mode="multiple"
-            style={{ width: "100%" }}
-            showSearch={true}
-            options={permissions?.map(permission => ({ label: permission.name, value: permission.value,  }))}
-          />
-        </Form.Item>
-      </Form>
+        initialValues={initialValues}
+      />
     </Modal>
-  )
-}
+  );
+};
 
 export default EditGroupModal;
---
+++ client/src/screens/Groups/modals/shared/GroupForm.tsx
@@ -0,0 +1,74 @@
+import { FC, useEffect } from "react";
+import IFormValues from "./IFormValues";
+import { Form, FormInstance, Input, Select } from "antd";
+import Group from "../../../../types/models/Group";
+import usePermissions from "../../../../hooks/usePermissions";
+import useGroups from "../../../../hooks/useGroups";
+
+interface GroupFormProps {
+  form: FormInstance<IFormValues>,
+  initialValues?: IFormValues
+}
+
+const validateGroupName = (groups: Group[], initialValue: string) => (_: any, value: string) => {
+  if (value === initialValue) return Promise.resolve();
+  if (groups.find((group) => group.name === value)) {
+    return Promise.reject("Group with this name already exists");
+  }
+  return Promise.resolve();
+};
+
+
+const GroupForm: FC<GroupFormProps> = ({form, initialValues}) => {
+
+  const { permissions } = usePermissions();
+  const { groups } = useGroups();
+
+  useEffect(() => {
+    if (!initialValues) return;
+    form.setFieldsValue(initialValues);
+  }, [initialValues]);
+
+  return (
+    <>
+      <Form
+        form={form}
+        layout="vertical"
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 20 }}
+        initialValues={initialValues}
+      >
+        <Form.Item
+          layout="vertical"
+          label="Group Name"
+          name="name"
+          rules={[
+            { max: 50, min: 4, required: true },
+            { validator: validateGroupName(groups, initialValues ? initialValues.name : "") }
+          ]}
+          labelCol={{ span: 24 }}
+          wrapperCol={{ span: 24 }}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          layout="vertical"
+          label="Group Permissions"
+          name="permissions"
+          rules={[{ required: true, type: "array", min: 1, message: "Please select at least one permission" }]}
+          labelCol={{ span: 24 }}
+          wrapperCol={{ span: 24 }}
+        >
+          <Select
+            mode="multiple"
+            style={{ width: "100%" }}
+            showSearch={true}
+            options={permissions?.map(permission => ({ label: permission.name, value: permission.value }))}
+          />
+        </Form.Item>
+      </Form>
+    </>
+  )
+}
+
+export default GroupForm;
