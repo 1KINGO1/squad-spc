add62ae70c4c20388721efc2a631cfc7697e7fc6
KINGO <spental12345@gmail.com>
2024-07-20 22:19:52 +0300

    Added shortkey suggestion to create button

--- client/src/components/CreateButton.tsx
+++ client/src/components/CreateButton.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 
 import { PlusOutlined } from "@ant-design/icons";
-import { FloatButton } from "antd";
+import { FloatButton, Popover } from "antd";
 
 import styles from "../screens/Clans/Clans.module.scss";
+import KeyboardKey from "./KeyboardKey";
 
 interface CreateButtonProps {
   onClick: () => void,
@@ -13,16 +14,38 @@ interface CreateButtonProps {
   icon?: React.ReactNode
 }
 
-const CreateButton: FC<CreateButtonProps> = ({onClick, icon, style, bottom = 30, right = 30}) => {
+const CreateButton: FC<CreateButtonProps> = ({ onClick, icon, style, bottom = 30, right = 30 }) => {
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.altKey && e.key === "c") {
+        onClick();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
-    <FloatButton icon={icon ?? <PlusOutlined />}
-                 type="primary"
-                 onClick={onClick}
-                 shape="square"
-                 style={{...style, position: "fixed", bottom: `${bottom}px`, right: `${right}px`}}
-                 className={styles.addClanButton}
-    />
-  )
-}
+    <Popover content={(
+      <>
+        <KeyboardKey>
+          Alt + C
+        </KeyboardKey>
+      </>
+    )}>
+      <FloatButton icon={icon ?? <PlusOutlined />}
+                   type="primary"
+                   onClick={onClick}
+                   shape="square"
+                   style={{ ...style, position: "fixed", bottom: `${bottom}px`, right: `${right}px` }}
+                   className={styles.addClanButton}
+      />
+    </Popover>
+  );
+};
 
 export default CreateButton;
---
+++ client/src/components/KeyboardKey.tsx
@@ -0,0 +1,12 @@
+import { FC, PropsWithChildren } from "react";
+import styles from "./KeyboardKey.module.scss";
+
+const KeyboardKey: FC<PropsWithChildren> = ({children}) => {
+  return (
+    <div className={styles.wrapper}>
+      {children}
+    </div>
+  )
+}
+
+export default KeyboardKey;
