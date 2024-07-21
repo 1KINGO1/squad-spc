aa8c1b78e7c76257f299e449fdd3652ec2f12f64
KINGO <spental12345@gmail.com>
2024-07-21 14:48:15 +0300

    Changed footer content

--- client/src/elements/Footer/Footer.tsx
+++ client/src/elements/Footer/Footer.tsx
@@ -6,12 +6,10 @@ const Footer = () => {
       <div className={styles.logoWrapper}>
         <img src="./assets/logo.svg" alt="Logo" />
       </div>
+      <p className={styles.brand}>MagicGear</p>
       <p className={styles.title}>Squad Permission Control (SPC)</p>
       <p className={styles.developedBy}>
-        Developed by <a href="https://github.com/1KINGO1/squad-spc" target="_SEJ" rel="noreferrer">KINGO</a>
-      </p>
-      <p className={styles.inspiredBy}>
-        Inspired by Squad_Whitelister
+        Developed by <a href="https://github.com/1KINGO1/squad-spc" target="_SEJ" rel="noreferrer">KINGO</a>   Inspired by Squad_Whitelister
       </p>
       <p className={styles.bottomText}>
         OpenSource © 2024
