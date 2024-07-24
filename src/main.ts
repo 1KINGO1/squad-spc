ad9495901af14f9939ba972e9ad25ce77e14960b
KINGO <spental12345@gmail.com>
2024-07-25 01:14:12 +0300

    Set default port to 3000

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initApp } from "./init-app";
-import config from "./config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   initApp(app);
-  await app.listen(config.PORT);
+  await app.listen(3000);
 }
 bootstrap();
